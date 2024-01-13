0eec22347490cf8a47946b93a7d2bd2dc85bb092
grunenwald <david.grunenwald@he-arc.ch>
2024-01-13 12:35:57 +0100

    UpdateLightTheme

--- js/generator-template.js
+++ js/generator-template.js
@@ -80,24 +80,49 @@ function theme_materialize_light(data)
 {
     return `
 <!DOCTYPE html>
-<html lang="${data.lang}">
-    <head>
-        <meta charset="utf-8">
-        <title>${data.title}</title>
-        <meta name="description" content="${data.description}">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="manifest" href="manifest.json">
-        <meta name="theme-color" content="${data.theme_color}">
-        <!-- MUST BE 192px SQUARE (OR 180px) -->
-        <link rel="apple-touch-icon" href="images/icons/icon-192x192.png">
-        <link rel="stylesheet" href="css/theme_materialize_light.css">
-        <link rel="stylesheet" href="css/materialize.min.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    </head>
+<html lang="fr" prefix="og: https://ogp.me/ns#">
+<head prefix="og: http://ogp.me/ns#">
+    <meta charset="utf-8">
+	<title>${data.title}</title>
+	<meta name="description" content="${data.description}">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="language" content="fr_FR">
+	<meta name="theme-color" content="${data.theme_color}">
+	
+	<meta property="og:url" content="00URL00">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="${data.title}">
+	<meta property="og:description" content="${data.description}">
+	<meta property="og:image" content="00Card_Img_1200x630URL00">
+	<meta property="og:image:secure_url" content="00Card_Img_1200x630URL00">
+	<meta property="og:image:type" content="image/jpeg">
+	
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:domain" content="00DOMAIN00">
+	<meta name="twitter:url" content="00URL00">
+	<meta name="twitter:title" content="${data.title}">
+	<meta name="twitter:description" content="${data.description}">
+	<meta name="twitter:image" content="00Card_Img_1200x630URL00g">
+	
+	<link rel="icon" type="image/jpeg" href="images/index.jpg">
+	<link rel="manifest" href="manifest.json">
+	<link rel="apple-touch-icon" href="images/icons/icon-192x192.png">
+	<link rel="stylesheet" href="css/theme_materialize_light.css">
+	<link rel="stylesheet" href="css/materialize.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cardo"> 
+
+	<style>
+		body {	
+			font-family:'Cardo', sans-serif;
+			background-color: #101010;
+			background-image: url(images/index.jpg);   
+			background-size: 100%;	
+			}
+	</style>
+</head>
+    
     <body class="black-text">
-
-        <header class="header red"></header>
-
         <main class="main">
             <div class="container">
                 <h1 class="main-title">${data.title}</h1>
@@ -105,45 +130,26 @@ function theme_materialize_light(data)
                     ${data.audioTree}
                 </div>
             </div>
+        <p><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.fr"><img src="https://licensebuttons.net/l/by-nc-sa/3.0/88x31.png" alt="Licence: CC BY-NC-SA 4.0 DEED"></a><br>
+        Download <a href="">all</a><br>
+        <a href="mailto:mulhouse@protonmail.com?subject=LeGID">Ecrire</a>Contact!<br></p>
         </main>
 
-        <footer class="page-footer black-text">
-            <div class="container center">
-                <div class="row">
-                    <div class="col s12">
-                        <p>Generated PWA example from :</p>
-                    </div>
-                    <div class="col s12">
-                        <p><a href="https://github.com/HE-Arc/pwa-playlist-generator" target="_blank" rel="noopener">PWA Playlist Generator</a></p>
-                    </div>
-                </div>
-            </div>
-        </footer>
 
-        <div id="player-container" class="grey darken-3 white-text center">
-            <audio id="audio-player" controls>
-                <source src="">
-                Your browser does not support the audio element.
-            </audio>
-
-            <ul id="player-actions">
-                <li class="player-action">
-                    <button id="audio-previous" class="btn-floating waves-effect waves-light red"><i class="material-icons">skip_previous</i></button>
-                </li>
-                <li class="player-action">
-                    <button id="audio-next" class="btn-floating waves-effect waves-light red"><i class="material-icons">skip_next</i></button>
-                </li>
-                <li class="player-action">
-                    <button id="audio-repeat" class="btn-floating waves-effect waves-light red"><i class="material-icons">repeat</i></button>
-                </li>
-                <li class="player-action">
-                    <button id="audio-random" class="btn-floating waves-effect waves-light red"><i class="material-icons">shuffle</i></button>
-                </li>
-            </ul>
-
-            <p id="current-audio-file">No sound</p>
-
+    <div id="player-container" class="grey darken-3 white-text center">
+        <div>
+            <button id="audio-previous" class="btn-small waves-effect waves-light black"><i class="material-icons">skip_previous</i></button>	            
+            <span id="current-audio-file">No sound</span>
+            <input id="audio-repeat" type="checkbox" name="audio-repeat">
+            <label for="audio-repeat"></label>
+            <button id="audio-next" class="btn-small waves-effect waves-light black"><i class="material-icons">skip_next</i></button>
+            <button id="audio-random" class="btn-small waves-effect waves-light black"><i class="material-icons">shuffle</i></button>
         </div>
+        <audio id="audio-player" controls>
+                Your browser does not support the audio element.
+        </audio>
+    </div>
+    <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
 
         <script src="service-worker.js"></script>
         <script src="pwa.js"></script>
